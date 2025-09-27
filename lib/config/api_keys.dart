3b964c1330d332142d210b8d8ada5d967d99927b
Sourav Jha <souravkjha2007@gmail.com>
2025-09-27 22:51:14 +0530

    Update apiKey placeholder in api_keys.dart

--- lib/config/api_keys.dart
+++ lib/config/api_keys.dart
@@ -1,2 +1,2 @@
-const String apiKey = "AIzaSyBBOf_wZorM5ztr53UceyKWp9Dd0SbRjfI";
+const String apiKey = "write your own apiKey here from https://developers.google.com/custom-search/v1/overview";
 const String contextKey = "10e58c4bbbc7a4b66";
